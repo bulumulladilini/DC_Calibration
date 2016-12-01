56003e07a8d24546bf0d1cb16786f9236e71782e
Olga Cortes <omcortes@Olgas-MacBook-Air.local>
2016-12-01 09:53:50 -0500

    initial commit message

---
+++ src/org/jlab/dc_monitoring/testApp.java
@@ -0,0 +1,11 @@
+package org.jlab.dc_monitoring;
+
+import org.jlab.dc_calibration.DC_Calib;
+
+public class testApp {
+	public static void main(String[] args) {
+		DC_Calib theDCcalib = new DC_Calib();
+
+		System.out.print("here");
+	}
+}
