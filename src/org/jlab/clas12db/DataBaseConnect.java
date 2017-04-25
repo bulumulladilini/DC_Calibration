214907f743e7418fe9a40aa8384682acb65e4c4c
mckunkel <mkunkel@jlab.org>
2017-04-25 19:22:30 +0200

    is everything added?

--- src/org/jlab/clas12db/DataBaseConnect.java
+++ src/org/jlab/clas12db/DataBaseConnect.java
@@ -17,12 +17,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-import javax.swing.JFrame;
-
-import org.jlab.detector.calib.utils.DatabaseConstantProvider;
-import org.jlab.utils.groups.IndexedTable;
-import org.jlab.utils.groups.IndexedTableViewer;
-
 public class DataBaseConnect {
 
 	private Connection connection;
@@ -32,7 +26,7 @@ public class DataBaseConnect {
 	public DataBaseConnect() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/udemy", "root", "");
 			statement = connection.createStatement();
 		} catch (Exception e) {
 			System.out.println("Error: " + e);
@@ -43,14 +37,18 @@ public class DataBaseConnect {
 	public void getData() {
 		try {
 
-			String query = "select * from hvpintolayer";
+			String query = "select * from students";
 			rSet = statement.executeQuery(query);
 			System.out.println("Records from Database");
 			while (rSet.next()) {
-				String colorLeft = rSet.getString("colorleft");
-				String colorRight = rSet.getString("colorright");
-				System.out.println("ColorLeft " + colorLeft + "|  ColorRight  " + colorRight);
+				// String colorLeft = rSet.getString("colorleft");
+				// String colorRight = rSet.getString("colorright");
+				// System.out.println("ColorLeft " + colorLeft + "| ColorRight " + colorRight);
+				int id = rSet.getInt("id");
+				String name = rSet.getString("name");
+				int age = rSet.getInt("age");
 
+				System.out.println(id + "  " + name + "  " + age);
 			}
 		} catch (Exception e) {
 			System.out.println("Error at getData: " + e);
@@ -61,24 +59,24 @@ public class DataBaseConnect {
 		DataBaseConnect connect = new DataBaseConnect();
 		connect.getData();
 
-		DatabaseConstantProvider provider = new DatabaseConstantProvider(10, "default");
-		IndexedTable table = provider.readTable("/test/fc/fadc");
-		// table.addConstrain(3, 0.0, 90.0);
-		provider.disconnect();
-		JFrame frame = new JFrame();
-		frame.setSize(600, 600);
-
-		/*
-		 * table.addRowAsDouble(new String[]{"21","7","1","0.5","0.1","0.6"}); table.addRowAsDouble(new String[]{"22","8","2","0.6","0.2","0.7"});
-		 * table.addRowAsDouble(new String[]{"23","9","3","0.7","0.3","0.8"}); table.addRowAsDouble(new String[]{"24","10","4","0.8","0.4","0.9"});
-		 */
-		// table.readFile("/Users/gavalian/Work/Software/Release-8.0/COATJAVA/coatjava/EC.table");
+		// DatabaseConstantProvider provider = new DatabaseConstantProvider(10, "default");
+		// IndexedTable table = provider.readTable("/test/fc/fadc");
+		// // table.addConstrain(3, 0.0, 90.0);
+		// provider.disconnect();
+		// JFrame frame = new JFrame();
+		// frame.setSize(600, 600);
+		//
+		// /*
+		// * table.addRowAsDouble(new String[]{"21","7","1","0.5","0.1","0.6"}); table.addRowAsDouble(new String[]{"22","8","2","0.6","0.2","0.7"});
+		// * table.addRowAsDouble(new String[]{"23","9","3","0.7","0.3","0.8"}); table.addRowAsDouble(new String[]{"24","10","4","0.8","0.4","0.9"});
+		// */
+		// // table.readFile("/Users/gavalian/Work/Software/Release-8.0/COATJAVA/coatjava/EC.table");
+		// // table.show();
+		// IndexedTableViewer canvas = new IndexedTableViewer(table);
+		// frame.add(canvas);
+		// frame.pack();
+		// frame.setVisible(true);
 		// table.show();
-		IndexedTableViewer canvas = new IndexedTableViewer(table);
-		frame.add(canvas);
-		frame.pack();
-		frame.setVisible(true);
-		table.show();
 
 	}
 
